ef5ea3ee044e0a75acc02c2a87d8edf735f0b3fb
Daniël Haitink <danielhaitink@gmail.com>
2015-10-12 20:19:41 +0200

    added funtions

---
+++ sibelImage.m
@@ -0,0 +1,19 @@
+function [ Fx, Fy ] = sibelImage( image )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+I = imread(image);
+F = im2double(I);
+hx = [-1, 0, 1;-1,0,1;-1,0,1]; % x derivative
+hy = [1,1,1 ;0,0,0 ;-1,-1,1]; % y derivative
+
+Fx = imfilter(F,hx,'replicate','conv');
+Fy = imfilter(F,hy, 'replicate', 'conv');
+h2 = [0,1,0;1,1,1;0,1,0]; %Dilation
+F3 = imdilate(F,h2); %Dilation
+
+
+figure, imshow(Fx)
+figure, imshow(Fy)
+
+end
+
