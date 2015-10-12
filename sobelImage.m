719ffb286d93bc7225f6a19052fccf07cd76b34b
Daniël Haitink <danielhaitink@gmail.com>
2015-10-12 20:24:55 +0200

    added more funtions

--- sobelImage.m
+++ sobelImage.m
@@ -1,10 +1,10 @@
-function [ Fx, Fy ] = sibelImage( image )
-%UNTITLED Summary of this function goes here
+function [ Fx, Fy ] = sobelImage( image )
+%UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 I = imread(image);
 F = im2double(I);
-hx = [-1, 0, 1;-1,0,1;-1,0,1]; % x derivative
-hy = [1,1,1 ;0,0,0 ;-1,-1,1]; % y derivative
+hx = [-1,-2,-1;0,0,0;1,2,1]; % x derivative
+hy = [-1,0,1;-2,0,2;-1,0,1]; % y derivative
 
 Fx = imfilter(F,hx,'replicate','conv');
 Fy = imfilter(F,hy, 'replicate', 'conv');
